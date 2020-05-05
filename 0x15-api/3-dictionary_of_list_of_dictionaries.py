7b68cb15ff527ec95b76994a025d811746628cf8
auraPasmin <1288@holbertonschool.com>
2020-05-04 23:59:46 -0500

    add 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" Exports data in the JSON format
+"""
+import json
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    users = requests.get(url).json()
+    data = {}
+    for user in users:
+        todo = requests.get(url + '{}/todos'.format(user['id'])).json()
+        data[user['id']] = [{
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': user['username']
+            } for task in todo]
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(data, f)
