6a100bcfbfe4f44dea841b75897c11323ecb18af
auraPasmin <1288@holbertonschool.com>
2020-05-04 23:56:43 -0500

    add 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+""" Module export to JSON
+"""
+import json
+import requests
+from sys import argv as av
+
+
+if __name__ == '__main__':
+
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(av[1])).json()
+    todo = requests.get(url + 'users/{}/todos'.format(av[1])).json()
+    data = {
+        av[1]:
+        [
+            {
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': user['username']
+            } for task in todo
+        ]
+    }
+    file = '{}.json'.format(av[1])
+    with open(file, 'w') as f:
+        json.dump(data, f)
