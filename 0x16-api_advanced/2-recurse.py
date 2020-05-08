4a24d46d818ae5a8276cea677fc83ea115c665a3
auraPasmin <1288@holbertonschool.com>
2020-05-07 20:40:40 -0500

    fix 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Module recurse
+"""
+from requests import *
+
+
+def recurse(subreddit, hot_list=[], after=""):
+
+    api = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
+    headers = {'User-Agent': 'auramps'}
+    response = get(api, headers=headers).json()
+    try:
+        for top in response.get('data')['children']:
+            hot_list.append(top['data']['title'])
+        if response['data']['after'] is not None:
+            recurse(subreddit, hot_list, response['data']['after'])
+        return hot_list
+    except Exception:
+        return None
