80fbc21b439402d680b3b69be106f4f5ab2c3462
auraPasmin <1288@holbertonschool.com>
2020-05-07 19:48:07 -0500

    fix 1 pep8 ok

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Module top ten
+"""
+from requests import *
+
+
+def top_ten(subreddit):
+
+    api = 'https://api.reddit.com/r/{}/hot?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Auramps'}
+    r = get(api, headers=headers).json()
+    try:
+        [print(top['data']['title']) for top in r['data']['children']]
+    except Exception:
+        print(None)
