e6b43457e3d89c51ef1136fe0b030d9aa7c09458
auraPasmin <1288@holbertonschool.com>
2020-05-07 19:15:33 -0500

    fix 0 pep8 ok

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""How many subs?"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "my name is Aura"}
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    try:
+        return r.json().get("data").get("subscribers")
+    except Exception:
+        return 0
