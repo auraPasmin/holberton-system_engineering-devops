ba479afd9930b0017f7cba4ca2cc54915cd60024
auraPasmin <1288@holbertonschool.com>
2020-05-23 15:56:54 -0500

    add 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,6 @@
-#!/usr/bin/env bash
+# remove a extra p.
+exec { 'fix':
+    command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
+    provider => shell,
+}
+
